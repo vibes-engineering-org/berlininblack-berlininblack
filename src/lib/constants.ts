a5b997ca41b28e037ebb0a8a9f9991084cf4fc92
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-04 19:50:12 +0000

    Code generation: 1326687e

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "BerlininBlack";
-export const PROJECT_DESCRIPTION = "by @berlininblack";
+export const PROJECT_DESCRIPTION = "Your guide to Berlin services - whether you're new to the city or a local looking for connections";
 export const PROJECT_CREATOR = "berlininblack";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
