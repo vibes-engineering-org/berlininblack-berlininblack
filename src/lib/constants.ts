826d6b2bf88a1e1e1624063e0194533683fce92e
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-04 19:40:28 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "BerlininBlack";
+export const PROJECT_DESCRIPTION = "by @berlininblack";
+export const PROJECT_CREATOR = "berlininblack";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
