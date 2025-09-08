8f32be85dd945de0562efa58d562c07ce693f37a
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-08 20:40:08 +0000

    Code generation: 6aad638d

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,10 +4,18 @@ import BerlinDirectory from "~/components/BerlinDirectory";
 
 export default function App() {
   return (
-    <div className="min-h-screen">
-      {/* TEMPLATE_CONTENT_START - Replace content below */}
-      <BerlinDirectory />
-      {/* TEMPLATE_CONTENT_END */}
+    <div 
+      className="min-h-screen bg-cover bg-center bg-no-repeat relative"
+      style={{
+        backgroundImage: 'url(/Siegessaeule_Aussicht_10-13_img4_Tiergarten.jpg)'
+      }}
+    >
+      <div className="absolute inset-0 bg-black/40"></div>
+      <div className="relative z-10">
+        {/* TEMPLATE_CONTENT_START - Replace content below */}
+        <BerlinDirectory />
+        {/* TEMPLATE_CONTENT_END */}
+      </div>
     </div>
   );
 }
